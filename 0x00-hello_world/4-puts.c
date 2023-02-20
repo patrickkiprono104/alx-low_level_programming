12069f802c203a7f798ac5cedde27e9b8d7804ab
patrickkiprono104 <patrickkiprono104@gmail.com>
2023-02-20 13:30:08 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - programs that prints Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
