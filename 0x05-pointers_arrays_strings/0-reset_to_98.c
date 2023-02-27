38a550eb172367b74e8b25dcb869c54a4d26b931
patrickkiprono104 <patrickkiprono104@gmail.com>
2023-02-27 09:38:35 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 
 /**
  * reset_to_98 - function definition
@@ -15,7 +15,7 @@ void reset_to_98(int *n);
  * Return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
