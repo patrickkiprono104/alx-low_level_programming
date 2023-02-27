745e25e1720dbb356b95f59f606e8a7fb5965089
patrickkiprono104 <patrickkiprono104@gmail.com>
2023-02-27 11:43:11 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer to the string of chech
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(s[i])
+	i++;
+
+	return (i);
+}
