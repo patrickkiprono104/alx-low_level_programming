1e7b102c302038f1f968b87b0b0ee7471666a36a
patrickkiprono104 <patrickkiprono104@gmail.com>
2023-02-28 13:17:21 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,16 @@
 
 void puts_half(char *str)
 {
-	int len, n i;
+	int len, n, i;
 
 	len = 0;
 
 	while
 		(str[len] != '\0');
 	{
+		len++;
+	}
+	if  (len % 2 == 0)
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
